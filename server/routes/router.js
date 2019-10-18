1e15e7a1f063afdfdeb0ddcdceaebc9c330571c4
Heather Walls <wallshkim@gmail.com>
2019-10-18 17:10:43 -0500

    Sets up router.js with express and connects to pool.js which connects the database

---
+++ server/routes/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// DATABASE CONNECTION
+const pool = require('../modules/pool.js');
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
