9b1b23f72b9f62d11c0a78b5e67a7340cead6268
Heather Walls <wallshkim@gmail.com>
2019-10-20 13:07:58 -0500

    Creates event listener and function for addTaskBtn

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,41 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQuery sourced');
-    
+    setupClickListeners();
 }// END onReady
+
+function setupClickListeners(){
+    $('#addTaskBtn').on('click', handleAddTask);
+} // END setupClickListeners
+
+
+function handleAddTask(){
+    console.log('in handleAddTask');
+    
+} // END handleAddTask
+
+function getTasks(){
+    console.log('in getTasks');
+    // ajax call to server to get tasks
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        renderTasks(response);
+    })
+} // END getTasks
+
+function renderTasks(array){
+    console.log('in renderTasks');
+    // clear existing table body
+    $('#taskTable').empty();
+
+    //loop through array
+    array.forEach(element => {
+        
+    });
+} // END renderTasks
+
+function clearInputs(){
+
+} // END clearInputs
